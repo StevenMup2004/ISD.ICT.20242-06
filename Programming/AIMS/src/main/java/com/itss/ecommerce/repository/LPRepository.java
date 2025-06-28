1fec36bf4478568e10f05a05ad70e6ffd838abea
BuiXuanSon1412 <bxson1412@gmail.com>
2025-06-28 23:46:00 +0700

    move source code inside submission folder

--- Programming/AIMS/src/main/java/com/itss/ecommerce/repository/LPRepository.java
+++
@@ -1,9 +0,0 @@
-package com.itss.ecommerce.repository;
-
-import com.itss.ecommerce.entity.LP;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface LPRepository extends JpaRepository<LP, Long> {
-}
